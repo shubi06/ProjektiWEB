5656539bb17f36471820c82e26c271038a3db95c
Shuajb Bunjaku <bunjakushuajb@gmail.com>
2024-01-19 17:59:17 +0100

    Lidhja me databaz dhe disa elemente te tjera

---
+++ components/dashboard.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+ 
+    <title>Grieta Hospital</title>
+  
+    
+</head>
+<style>
+    .header {
+  padding: 10px 80px;
+  background-color: #1C9FA7;
+
+
+}
+
+
+.navigimi {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+}
+
+
+.navigimi ul {
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  align-content: center;
+  align-items: center;
+}
+
+.navigimi ul>li {
+  padding: 10px;
+}
+
+.navigimi a {
+  font-size: 1.2em;
+  font-family: 'Campton', sans-serif;
+  text-decoration: none;
+  color: white;
+}
+</style>
+<body>
+ 
+    <header class="header" >
+        <div class="navigimi">
+        <div class="nav-start">
+       
+    
+            <ul>
+
+               <li>   <a href="index.html"><img  style="width: 70px; height: 60px;" src="logo-bardh.png" alt=""></a> </li>
+               <li>   <a href="index.html">DASHBOARD</a> </li>
+
+             </ul>
+
+
+
+
+    </header>
+</html>
